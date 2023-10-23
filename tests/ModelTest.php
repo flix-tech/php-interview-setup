4ac53dfa213776292b3c208ce5a0a312c7f28af3
Ivan Pushkin <ivan.pushkin@flixbus.com>
2023-10-23 11:03:47 +0200

    Initial commit

---
+++ tests/ModelTest.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class ModelTest extends TestCase
+{
+    public function test_testing_something(): void
+    {
+        $this->assertSame(1, 1);
+    }
+}
