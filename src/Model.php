8d54e3840156bb6c873b8a18d1880572f9933da7
Ivan Pushkin <ivan.pushkin@flixbus.com>
2023-11-22 10:10:20 +0100

    fix autoloading

--- src/Model.php
+++ src/Model.php
@@ -1,8 +1,10 @@
 <?php declare(strict_types=1);
 
+namespace App;
+
 final class Model
 {
-    private function __construct()
+    public function __construct()
     {
     }
 }
--- tests/ModelTest.php
+++ tests/ModelTest.php
@@ -1,11 +1,15 @@
 <?php declare(strict_types=1);
 
+namespace App\Tests;
+
+use App\Model;
 use PHPUnit\Framework\TestCase;
 
 final class ModelTest extends TestCase
 {
     public function test_testing_something(): void
     {
-        $this->assertSame(1, 1);
+        $model = new Model();
+        $this->assertInstanceOf(Model::class, $model);
     }
 }
